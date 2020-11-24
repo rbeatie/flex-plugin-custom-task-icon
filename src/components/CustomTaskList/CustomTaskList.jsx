155e9fa8db529b9848108ec81c493aa8557ae676
rbeatie <rbeatie@TwilioRexAsset04.home>
2020-11-24 15:20:06 -0800

    u-pdating package.json, scripts, and dependancies

--- src/components/CustomTaskList/CustomTaskList.jsx
+++ src/components/CustomTaskList/CustomTaskList.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 import { CustomTaskListComponentStyles } from './CustomTaskList.Styles';
 
 // It is recommended to keep components stateless and use redux for managing states
-
 const CustomTaskList = (props) => {
-
   if (!props.isOpen) {
     return null;
   }
